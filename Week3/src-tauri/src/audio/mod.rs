b440444dc188dcb9d42960a005a4a119305ceb1e
ZenRay <jake2011ren@gmail.com>
2026-01-27 19:26:21 +0800

    chore Remove Week3 project to rebuild from scratch.

--- Week3/src-tauri/src/audio/mod.rs
+++
@@ -1,9 +0,0 @@
-// Audio processing module
-
-pub mod buffer;
-pub mod capture;
-pub mod resampler;
-
-pub use buffer::{AudioBuffer, BufferError};
-pub use capture::{AudioCapture, CaptureConfig, CaptureError, StreamState, list_input_devices};
-pub use resampler::{AudioResampler, ResamplerConfig, ResamplerError};
--- Week3/src-tauri/src/network/mod.rs
+++
@@ -1,9 +0,0 @@
-// Network communication module
-
-pub mod client;
-pub mod protocol;
-pub mod state_machine;
-
-pub use client::{ClientError, ScribeClient, WsStream};
-pub use protocol::{ClientMessage, ConnectionConfig, ProtocolError, ServerMessage, SessionConfig};
-pub use state_machine::{ConnectionState, StateError, StateMachine};
